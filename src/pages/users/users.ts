ba2ed913cae124449381efa20620f1c8dfc3c3a7
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-16 15:09:26 -0400

    Navigation Options (animation)

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -12,6 +12,10 @@ export class UsersPage {
   constructor (private navCtrl: NavController) {}
 
   onLoadUser( name: string) {
-    this.navCtrl.push(UserPage, {userName: name});
+    this.navCtrl.push(UserPage, {userName: name}, {
+      direction: 'back', // default for push is 'forward'
+      duration: 2000, // 2 seconds
+      easing: 'ease-out'
+    });
   }
 }

3b33441c04954c8577f6da5c0eb9f35e1d34fca3
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-16 13:07:00 -0400

    page created

---
+++ src/pages/users/users.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class UsersPage {
+
+}

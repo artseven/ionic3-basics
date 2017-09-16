0f2cb3bdcb2c9d995789f160a9c8aee0da2e5594
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-16 15:07:05 -0400

    navPush and navPop directives

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,11 @@
 import { UsersPage } from '../users/users';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-  
-  onGoToUsers() {
-    this.navCtrl.push(UsersPage);
-
-  }
+  usrPage = UsersPage;
 }

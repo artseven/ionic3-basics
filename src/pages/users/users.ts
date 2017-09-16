b68dbfa55522773ff458c1b5184a0f33241f5739
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-16 13:37:49 -0400

    passing data between pages

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,12 +1,17 @@
+import { UserPage } from './user/user';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 
-@IonicPage()
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
-export class UsersPage {
 
+export class UsersPage {
+  constructor (private navCtrl: NavController) {}
+
+  onLoadUser( name: string) {
+    this.navCtrl.push(UserPage)
+  }
 }
